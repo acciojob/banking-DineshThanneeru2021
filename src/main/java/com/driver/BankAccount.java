8de78e251269032794bf880d6317f82ca93d691b
DineshThanneeru2021 <dineshthanneeru@gmail.com>
2023-07-28 17:48:27 +0530

    code done

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -1,4 +1,5 @@
 package com.driver;
+import java.util.*;
 
 public class BankAccount {
 
@@ -6,25 +7,110 @@ public class BankAccount {
     private double balance;
     private double minBalance;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
+    }
+
+    public void setMinBalance(double minBalance) {
+        this.minBalance = minBalance;
+    }
+
+    public BankAccount() {
+    }
+
     public BankAccount(String name, double balance, double minBalance) {
+        this.name=name;
+        this.balance=balance;
+        this.minBalance=minBalance;
 
     }
 
-    public String generateAccountNumber(int digits, int sum) throws Exception{
+    public String generateAccountNumber(int digits, int sum) throws Exception {
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
+        String ans="";
+        int[] res = new int[digits];
+        if (sum == 0) {
+                if (digits == 1)
+                    return "0";
+                else
+                    throw new Exception("Account Number can not be generated");
 
-        return null;
+            }
+
+
+
+        else {
+
+                if (sum > (9 * digits))
+                    throw new Exception("Account Number can not be generated");
+
+
+
+
+
+            // deduct sum by one to account for cases later
+            // (There must be 1 left for the most significant
+            //  digit)
+            sum -= 1;
+
+            // Fill last m-1 digits (from right to left)
+            for (int i = digits - 1; i > 0; i--) {
+                // If sum is still greater than 9,
+                // digit must be 9
+                if (sum > 9) {
+                    res[i] = 9;
+                    sum -= 9;
+                }
+                else {
+                    res[i] = sum;
+                    sum = 0;
+                }
+            }
+
+        }
+        ans=Arrays.toString(res);
+        return ans;
     }
+
+
+
 
     public void deposit(double amount) {
         //add amount to balance
+        this.balance+=amount;
 
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
+
+          if(this.balance-amount<this.minBalance)
+            throw new Exception("Insufficient Balance");
+          else
+              this.balance=this.balance-amount;
+
+
+
+
+
 
     }
 
--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -5,6 +5,18 @@ public class CurrentAccount extends BankAccount{
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
+        super(name,balance,5000);
+        this.tradeLicenseId=tradeLicenseId;
+        if(balance<5000)
+        {
+            throw new Exception("Insufficient Balance");
+        }
+
+
+
+
+
+
 
     }
 
@@ -13,7 +25,95 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
+        if(!isLicenseValid(tradeLicenseId))
+        {
+            String reArrangeString=rearrange(tradeLicenseId);
+            if(reArrangeString=="")
+                throw new Exception("Valid License can not be generated");
+            else {
+                this.tradeLicenseId=reArrangeString;
+
+            }
+
+        }
+
+
 
     }
+    public char getMaxCountChar(int[] count)
+    {
+        int max=0;
+        char ch=0;
+        for(int i=0;i<26;i++)
+        {
+            if(count[i]>max)
+            {
+                max=count[i];
+                ch=(char)((int)'A'+i);
+            }
+        }
+        return ch;
 
+    }
+    public String rearrange(String s)
+    {
+        int n=s.length();
+        int[] count=new int[26];
+        for(int i=0;i<s.length();i++)
+        {
+            count[(int)s.charAt(i)-(int)'A']++;
+        }
+        char max_char=getMaxCountChar(count);
+        int max_count=count[(int)max_char-(int)'A'];
+
+        if(max_count>(n+1)/2)
+        {
+            return "";
+        }
+
+        String res="";
+
+        for(int i=0;i<n;i++)
+        {
+            res+=' ';
+        }
+
+        int index=0;
+         while(max_count>0)
+         {
+             res+=res.substring(0,index)+max_char+res.substring(index+1);
+             index+=2;
+             max_count--;
+         }
+
+         count[(int)max_char-(int)'A']=0;
+         for(int i=0;i<26;i++)
+         {
+             while(count[i]>0)
+             {
+                 index+=(i>=n)?1:index;
+                 res+=res.substring(0,index)+(char)((int)'A'+i)+res.substring(index+1);
+                 index+=2;
+                 count[i]--;
+             }
+         }
+         return res;
+
+    }
+    public boolean isLicenseValid(String tradeLicenseId)
+    {
+        for(int i=0;i<tradeLicenseId.length()-1;i++)
+        {
+            for(int j=i+1;j<tradeLicenseId.length();j++)
+            {
+                if(tradeLicenseId.charAt(i)==tradeLicenseId.charAt(j))
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public String getTradeLicenseId() {
+        return tradeLicenseId;
+    }
 }
